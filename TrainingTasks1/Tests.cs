89927f41879cd56f01f81f267213a29ec4167179
Adam Schroder <adamschroder@gmail.com>
2012-03-22 11:25:51 +1100

    Initial Load

---
+++ TrainingTasks1/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TrainingTasks1
+{
+    [TestFixture]
+    public class Tests
+    {
+        // 1. Should implement 3 rules (must have a number, must have an uppercase letter, must be min of 10 in length). More may be needed later.
+        // 2  A Blank password is not valid
+        // 3. Tests should cover all functionality
+        // 4. SOLID principles should be adhered to
+
+        [Test]
+        public void A_Blank_Password_Is_Not_Valid()
+        {
+            IPasswordValidator validator = null;
+            var result = validator.Validate("");
+
+            Assert.False(result);
+        }
+    }
+}
