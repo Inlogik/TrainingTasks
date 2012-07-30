f1a701fb2ea8be312856fedcecc896c2646e986b
Adam Schroder <adamschroder@gmail.com>
2012-07-30 15:56:20 +1000

    Modify MenuConfig

--- TrainingTasks3/MenuConfig.cs
+++ TrainingTasks3/MenuConfig.cs
@@ -1,6 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace TrainingTasks3
 {
     public class MenuConfig
     {
+        public MenuConfig()
+        {
+            StaticMenuItems = new List<MenuItem>();
+            DynamicMenuItemsFunc = x => Enumerable.Empty<MenuItem>();
+            IsVisibleFunc = (x, y) => true;
+        }
+
+        public List<MenuItem> StaticMenuItems { get; set; }
+        public Func<MenuContext, IEnumerable<MenuItem>> DynamicMenuItemsFunc { get; set; }
+        public Func<MenuContext, MenuItem, bool> IsVisibleFunc { get; set; }
     }
 }
