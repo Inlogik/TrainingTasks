489114a02d424b619f552aeb9855672f7a35a822
Adam Schroder <adamschroder@gmail.com>
2012-08-07 15:54:39 +1000

    Write tests for basic Html Generator

---
+++ TrainingTasks4/HtmlTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TrainingTasks4
+{
+    [TestFixture]
+    public class HtmlTests
+    {
+        [Test]
+        public void BasicHtmlBuilder()
+        {
+            Html html = Html.Tag("div").Attr("test", "testvalue");
+            Assert.AreEqual("<div test=\"testvalue\"></div>", html.ToString());
+        }
+
+        [Test]
+        public void BasicHtmlBuilderMultipleAttributes()
+        {
+            Html html = Html.Tag("div").Attr("test", "testvalue").Attr("test1", "testvalue1");
+            Assert.AreEqual("<div test=\"testvalue\" test1=\"testvalue1\"></div>", html.ToString());
+        }
+
+        [Test]
+        public void BasicHtmlBuilderAddClass()
+        {
+            Html html = Html.Tag("span").Attr("test", "testvalue").AddClass("wow");
+            Assert.AreEqual("<span test=\"testvalue\" class=\"wow\"></span>", html.ToString());
+        }
+
+        [Test]
+        public void BasicHtmlBuilderModify()
+        {
+            Html html = Html.Tag("div").Attr("test", "testvalue").Modify(x => x.AddClass("wow"));
+            Assert.AreEqual("<div test=\"testvalue\" class=\"wow\"></div>", html.ToString());
+        }
+
+        [Test]
+        public void BasicHtmlBuilderWithoutToString()
+        {
+            Html html = Html.Tag("span").Attr("test", "testvalue");
+            Assert.AreEqual("<span test=\"testvalue\"></span>", html);
+        }
+    }
+}
